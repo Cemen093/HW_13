f66f03e28fb6fd662ca5f990c722133c04698e83
Cemen093 <CemenP93@gmail.com>
2021-05-28 11:30:24 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "blog/index.html")
+
+
+def post(request):
+    return render(request, "blog/post.html")
